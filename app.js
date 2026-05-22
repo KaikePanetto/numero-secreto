f7e3aa6f40f6dacc5e2278f9aa6e60dca205f066
Kaike Corrêa Panetto <kaikepanetto@gmail.com>
2026-05-22 00:43:02 -0300

    adicionei o readme

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 let listaDeNumerosSorteados = [];
-<<<<<<< HEAD
 let numeroLimite = 1000;
-=======
-let numeroLimite = 100;
->>>>>>> 36fbb2abda93d0d0b0b2ed5e620ea2827ccd8c09
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
